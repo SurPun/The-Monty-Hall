25e01a6558c9245f5be07b02ef80b98f08b87929
Suraj Pun <s_pun@outlook.com>
2022-11-20 16:31:41 +0000

    Game Mode + Local Storage

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 /* ------------------------------------- Selectors ------------------------------------- */
-// Switch / Keep Buttons ---
 const btn = document.querySelector("#btn");
 const switchBtn = document.getElementById("switch");
 const keepBtn = document.getElementById("keep");
+const playAgain = document.getElementById("play-again");
+const displayStats = document.querySelector(".displayStats");
 
 /* ------------------------------------- Modal ------------------------------------- */
 // Get the MODAL ---
@@ -35,6 +36,26 @@ window.onclick = function (e) {
 const simContainer = document.getElementById("simulation");
 const doors = [];
 
+// let gameStats = {
+//   played: 0,
+//   switchPlay: 0,
+//   keepPlay: 0,
+// };
+
+// Stats Function that takes in localStorage Data and displays in Html
+const gameStats = function (played, keepWin, keepLose, switchWin, switchLose) {
+  return (displayStats.innerHTML = `
+  <h4>Keep</h4>
+  <p>Win: ${keepWin} | Lose: ${keepLose}<p>
+    
+  <h4>Switch</h4>
+  <p>Win: ${switchWin} | Lose: ${switchLose}</p>
+
+  <h4>Games Played: ${played}<h4>
+`);
+};
+
+// Setup ---
 function setup() {
   for (let i = 0; i < 3; i++) {
     // Create a new div and add Goat for every door (3)
@@ -57,10 +78,23 @@ function setup() {
   doors[winningDoor].innerHTML = `
      <p class="prize">🚗</p>
   `;
+
+  // Display Status
+  let played = localStorage.getItem("played") || 0;
+
+  let keepWin = localStorage.getItem("keepWin") || 0;
+  let keepLose = localStorage.getItem("keepLose") || 0;
+
+  let switchWin = localStorage.getItem("switchWin") || 0;
+  let switchLose = localStorage.getItem("switchLose") || 0;
+
+  // Display Stats
+  gameStats(played, keepWin, keepLose, switchWin, switchLose);
 }
 
 // Reveal ---
 function reveal() {
+  let pickedDoor = doors[this.index];
   this.style.backgroundColor = "green";
   // this.innerHTML = this.innerText;
 
@@ -79,15 +113,97 @@ function reveal() {
   // Display Options ---
   btn.style.display = "block";
 
-  // Switch Function
+  // Switch Function ---------------------------
   switchBtn.addEventListener("click", function () {
-    console.log("Sucess");
+    if (pickedDoor.innerText === "🐐") {
+      console.log("You Win");
+      playAgain.style.display = "block";
+
+      let played = localStorage.getItem("played") || 0;
+      played++;
+      localStorage.setItem("played", played);
+
+      let switchWin = localStorage.getItem("switchWin") || 0;
+      switchWin++;
+      localStorage.setItem("switchWin", switchWin);
+
+      let keepWin = localStorage.getItem("keepWin") || 0;
+      let keepLose = localStorage.getItem("keepLose") || 0;
+
+      let switchLose = localStorage.getItem("switchLose") || 0;
+
+      // Display Stats
+      gameStats(played, keepWin, keepLose, switchWin, switchLose);
+    } else if (pickedDoor.innerText === "🚗") {
+      console.log("You Lose");
+      playAgain.style.display = "block";
+
+      let played = localStorage.getItem("played") || 0;
+      played++;
+      localStorage.setItem("played", played);
+
+      let switchLose = localStorage.getItem("switchLose") || 0;
+      switchLose++;
+      localStorage.setItem("switchLose", switchLose);
+
+      let keepWin = localStorage.getItem("keepWin") || 0;
+      let keepLose = localStorage.getItem("keepLose") || 0;
+
+      let switchWin = localStorage.getItem("switchWin") || 0;
+
+      // Display Stats
+      gameStats(played, keepWin, keepLose, switchWin, switchLose);
+    }
   });
 
   // Keep Function
   keepBtn.addEventListener("click", function () {
-    console.log("Sucess");
+    if (pickedDoor.innerText === "🚗") {
+      console.log("You Win");
+      playAgain.style.display = "block";
+
+      let played = localStorage.getItem("played") || 0;
+      played++;
+      localStorage.setItem("played", played);
+
+      let keepWin = localStorage.getItem("keepWin") || 0;
+      keepWin++;
+      localStorage.setItem("keepWin", keepWin);
+
+      let keepLose = localStorage.getItem("keepLose") || 0;
+
+      let switchWin = localStorage.getItem("switchWin") || 0;
+      let switchLose = localStorage.getItem("switchLose") || 0;
+
+      // Display Stats
+      gameStats(played, keepWin, keepLose, switchWin, switchLose);
+    } else {
+      console.log("You Lose");
+      playAgain.style.display = "block";
+
+      let played = localStorage.getItem("played") || 0;
+      played++;
+      localStorage.setItem("played", played);
+
+      let keepLose = localStorage.getItem("keepLose") || 0;
+      keepLose++;
+      localStorage.setItem("keepLose", keepLose);
+
+      let keepWin = localStorage.getItem("keepWin") || 0;
+
+      let switchWin = localStorage.getItem("switchWin") || 0;
+      let switchLose = localStorage.getItem("switchLose") || 0;
+
+      // Display Stats
+      gameStats(played, keepWin, keepLose, switchWin, switchLose);
+    }
   });
+
+  playAgain.addEventListener("click", function () {
+    location.reload();
+  });
+
+  console.log(pickedDoor.innerText);
 }
 
 /* --------------------------------------------------------------------------------- */
